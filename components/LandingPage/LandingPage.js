a9c293fff40558900ad4b165a85d6922347bee2c
adityayadav3005 <adityayadav300645@gmail.com>
2024-01-18 23:26:21 +0530

    Landing Page Structure

---
+++ components/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from './LandingPage.module.css'
+
+function LandingPage() {
+  return (
+    <div className={Styles.example}>Landing PAge</div>
+  )
+}
+
+export default LandingPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Appbar from "../../components/Appbar/Appbar";
+import LandingPage from "../../components/LandingPage/LandingPage";
 
 export default function Home() {
     return (
         <main>
             <Appbar></Appbar>
+            <LandingPage></LandingPage>
         </main>
     );
 }
