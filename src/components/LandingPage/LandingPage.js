445fe89e7b22bc6f7ec1c0adce6bb0168c4ef30a
ritvikbharti <bhartiritvik1000@gmail.com>
2024-01-19 01:56:25 +0530

    Icon

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,7 +6,11 @@ function LandingPage() {
   return (
     <div className={Styles.bgCard}>Landing PAge
       <div className={Styles.searchBar}> 
-        <div className={Styles.icon}>Q</div>
+        <div className={Styles.icon}>
+            <div className={Styles.innerIcon}>
+                <div className={Styles.inner2Icon}> </div>
+            </div>
+        </div>
         <input className={Styles.innerbgCard} placeholder='Sexxy'/>
         <button className={Styles.btn}>search</button>
       </div>
