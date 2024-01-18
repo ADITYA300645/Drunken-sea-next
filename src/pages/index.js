5e51f5d26445fdef2bffa8ac9a25967dab7b2a57
adityayadav3005 <adityayadav300645@gmail.com>
2024-01-19 00:00:15 +0530

    AppBar Fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import { Inter } from "next/font/google";
 import Appbar from "../../components/Appbar/Appbar";
 import LandingPage from "../../components/LandingPage/LandingPage";
 
+
 export default function Home() {
+
     return (
         <main>
             <Appbar></Appbar>
