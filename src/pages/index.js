ed48b95b0b5bbe7870614d6cd8f8e88ff22d5321
adityayadav3005 <adityayadav300645@gmail.com>
2024-01-18 20:15:55 +0530

    Appbar Design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Appbar from "../../components/Appbar/Appbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`} >
-            hellow
+            <Appbar></Appbar>
         </main>
     );
 }
