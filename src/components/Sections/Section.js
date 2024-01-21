ecc5f9aa197086b871fcae4515b46357f41ba0dd
adityayadav3005 <adityayadav300645@gmail.com>
2024-01-22 00:17:33 +0530

    Wine Bar Scroll CLICK PUSH

--- src/components/Sections/Section.js
+++ src/components/Sections/Section.js
@@ -1,13 +1,100 @@
-import React from 'react'
+import React from "react";
+import WineCard from "../WineCard/WineCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 
-function Section({tagline,discription}) {
-  return (
-    <div className='px-4 my-4'>
-        <h2 className="text-3xl font-bold text my-2">{tagline}</h2>
-        <h3 className="text-sm text">{discription}</h3>
-
-    </div>
-  )
+function Section({ tagline, discription }) {
+    const arr = [
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2014/12/12/22/08/glass-565914_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2020/06/21/02/31/wine-5323009_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2018/02/25/11/17/wine-3180220_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2018/01/19/17/44/wine-3092944_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2020/06/21/02/31/wine-5323009_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2018/02/25/11/17/wine-3180220_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2018/01/19/17/44/wine-3092944_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2020/06/21/02/31/wine-5323009_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2018/02/25/11/17/wine-3180220_1280.jpg",
+        },
+        {
+            Price: 999,
+            WineName: "Maza",
+            WineType: "Mango",
+            imageUrl:
+                "https://cdn.pixabay.com/photo/2018/01/19/17/44/wine-3092944_1280.jpg",
+        },
+    ];
+    return (
+        <div className="pl-4 pr-1 my-4 relative">
+            <h2 className="text-3xl font-bold text mt-6 mx-3">{tagline}</h2>
+            <h3 className="text-sm text mb-5 mx-3">{discription}</h3>
+            <div className="flex overflow-x-auto snap-x">
+                {arr.map((item) => (
+                    <WineCard
+                        imageUrl={item.imageUrl}
+                        WineName={item.WineName}
+                        WineType={item.WineType}
+                        Price={item.Price}
+                    />
+                ))}
+            </div>
+            <button className="absolute right-0 bg-slate-600 bg-opacity-65 py-8 px-4 top-[40%]">
+            <Image src="assets/Next.svg" width={20} height={20}></Image>
+            </button>
+        </div>
+    );
 }
 
-export default Section
+export default Section;
--- src/components/WineCard/WineCard.js
+++ src/components/WineCard/WineCard.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function WineCard() {
-  return (
-    <div>WineCard</div>
-  )
+function WineCard({ imageUrl, WineName, WineType, Price }) {
+    return (
+        <div className="w-72 mx-3 scroll-ms-6 snap-start ">
+            <div className="w-72 h-96 rounded-md overflow-hidden justify-center items-center flex shadow-lg">
+              <img src={imageUrl}></img>
+            </div>
+            <div className="flex justify-between pt-1 px-1">
+                <div className="text-sm w-3/4">
+                    <div className="text-base font-medium">{WineName}</div>
+                    <div className="text-[12px]">{WineType}</div>
+                </div>
+                <div className="text-sm w-1/4 text-right text-green-600">
+                    ${Price}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default WineCard
+export default WineCard;
