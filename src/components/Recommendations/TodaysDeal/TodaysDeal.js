ebb23f4f7c7ff2ca0daf0e070796a44c0a63f9f1
ritvikbharti <bhartiritvik1000@gmail.com>
2024-01-22 02:05:30 +0530

    Todaysdeals

--- src/components/Recommendations/TodaysDeal/TodaysDeal.js
+++ src/components/Recommendations/TodaysDeal/TodaysDeal.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Styles from "./TodaysDeal.module.css"
 
 function TodaysDeal() {
   return (
-    <div>TodaysDeal</div>
+    <div className={Styles.todaysDeals}>
+       <div className={Styles.content}>
+      <p className={Styles.para1}> Today’s Deals</p>
+      <p className={Styles.para2}> Make your day beautiful and precious after a sip of beer
+      </p>
+
+       </div>
+       <div className={Styles.productContainer}>
+        <div className={Styles.product}>
+          <div className={Styles.productImage}>
+            
+          </div>
+          <div className={Styles.productDescription}>
+            <div className={Styles.Discount}><p>Up to 50% Discount</p></div>
+            <div className={Styles.Rate}><p className={Styles.price}>$999</p>
+            <p className={Styles.originalRate}>$499 Only</p></div>
+          </div>
+        </div>
+
+       </div>
+    </div>
   )
 }
 
