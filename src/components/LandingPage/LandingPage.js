92cdb578c2640aa7eeb3d6b0988664b6afda3692
ritvikbharti <bhartiritvik1000@gmail.com>
2024-01-21 15:47:04 +0530

    downArrow

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Styles from './LandingPage.module.css'
 import { ST } from 'next/dist/shared/lib/utils'
+import Image from "next/image"
 
 function LandingPage() {
   return (
@@ -11,9 +12,16 @@ function LandingPage() {
                 <div className={Styles.inner2Icon}> </div>
             </div>
         </div>
-        <input className={Styles.innerbgCard} placeholder='Sexxy'/>
+        <input className={Styles.innerbgCard} placeholder='Search'/>
         <button className={Styles.btn}>search</button>
       </div>
+      <div className={Styles.pageContent}>
+        <p className={Styles.para1}>Treat it like never before</p>
+        <p className={Styles.para2}>Sip, savor, smile – the simple joy of a good beer."</p> </div>
+      < div className={Styles.readMore}>
+        <div className={Styles.downArrow}> 
+        <Image src={'/Logo/DownArrow.svg'}  height={24} width={24}></Image></div> 
+        </div>
     </div>
   )
 }
